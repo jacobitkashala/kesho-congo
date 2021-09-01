a3453e5231ef90c0aac76ff0734d91b4c66957a3
jacobitkashala <jacobitkashala@gmail.com>
2021-09-01 10:16:37 +0200

    add:services web

---
+++ src/redux/constants/ActionTypes.js
@@ -0,0 +1,10 @@
+export const ADD_USER = 'ADD_USER';
+export const DELETE_USER = 'DELETE_USER';
+export const EDIT_USER = 'EDIT_USER';
+export const LOGIN_USER = 'LOGIN_USER';
+
+// Pour le patient
+export const ADD_PATIENT = 'ADD_PATIENT';
+export const DELETE_PATIENT = 'DELETE_PATIENT';
+export const ADD__PARAMETRE = 'ADD__PARAMETRE';
+export const SHOW_ALL_PATIENT = 'SHOW_ALL_PATIENT';
