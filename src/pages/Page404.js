7006be7a1b4a94baa3c5f4081f6c65743b6275ee
jacobitkashala <jacobitkashala@gmail.com>
2021-09-03 16:29:47 +0200

    add: page 404

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -27,13 +27,10 @@ export default function Page404() {
           <Box sx={{ maxWidth: 480, margin: 'auto', textAlign: 'center' }}>
             <motion.div variants={varBounceIn}>
               <Typography variant="h3" paragraph>
-                Sorry, page not found!
+                Désoler ,cette page n'existe pas
               </Typography>
             </motion.div>
-            <Typography sx={{ color: 'text.secondary' }}>
-              Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL?
-              Be sure to check your spelling.
-            </Typography>
+            <Typography sx={{ color: 'text.secondary' }}>page introuvable</Typography>
             <Button to="/" size="large" variant="contained" component={RouterLink}>
               Go to Home
             </Button>
